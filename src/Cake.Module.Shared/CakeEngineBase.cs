91326f7bb63c11a2ded74552876c69dc233fb112
David Gardiner <david@gardiner.net.au>
2018-08-31 15:10:51 +0930

    Support Cake.Core 0.30.0
    - ICakeEngine had a breaking change in method RunTargetAsync

--- src/Cake.Module.Shared/CakeEngineBase.cs
+++ src/Cake.Module.Shared/CakeEngineBase.cs
@@ -63,13 +63,13 @@ namespace Cake.Module.Shared
         /// </summary>
         /// <param name="context">The context.</param>
         /// <param name="strategy">The execution strategy.</param>
-        /// <param name="target">The target to run.</param>
+        /// <param name="settings">The execution settings.</param>
         /// <returns>
         /// The resulting report.
         /// </returns>
-        public Task<CakeReport> RunTargetAsync(ICakeContext context, IExecutionStrategy strategy, string target)
+        public Task<CakeReport> RunTargetAsync(ICakeContext context, IExecutionStrategy strategy, ExecutionSettings settings)
         {
-            return _engine.RunTargetAsync(context, strategy, target);
+            return _engine.RunTargetAsync(context, strategy, settings);
         }
 
         /// <summary>
