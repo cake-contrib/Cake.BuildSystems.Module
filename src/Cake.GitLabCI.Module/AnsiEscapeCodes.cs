9b097a0a56731621382f2b48138b0266b8884834
Andreas Grünwald <andreas@grynwald.net>
2024-08-30 21:59:03 +0200

    In GitLab CI, render Cake tasks as collapsible sections
    Extend module for GitLab CI with a custom CakeEngine that adds collapsible sections to the GitLab CI log (analogous to the groups in GitHubActionsEngine)
    
    For reference on the collapsible sections in GitLab CI, refer to https://docs.gitlab.com/ee/ci/yaml/script.html#custom-collapsible-sections

--- src/Cake.GitLabCI.Module/AnsiEscapeCodes.cs
+++ src/Cake.GitLabCI.Module/AnsiEscapeCodes.cs
@@ -9,6 +9,7 @@ namespace Cake.GitLabCI.Module
         public static readonly string ForegroundDarkGray = string.Format(FORMAT, 90);
         public static readonly string BackgroundMagenta = string.Format(FORMAT, 45);
         public static readonly string BackgroundRed = string.Format(FORMAT, 41);
+        public static readonly string SectionMarker = "\u001B[0K";
 
         private const string FORMAT = "\u001B[{0}m";
     }
